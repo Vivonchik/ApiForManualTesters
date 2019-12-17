32fb85b32fa0314d77072bea3156546c4b89454c
Vivonchik <43283073+Vivonchik@users.noreply.github.com>
2019-12-17 23:59:14 +0300

    Init repo

---
+++ Manual.Testers.Api/Manual.Testers.Api.Host/Models/Good.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Manual.Testers.Api.Host.Models
+{
+    /// <summary>
+    /// Товар.
+    /// </summary>
+    public class Good
+    {
+        /// <summary>
+        /// Имя товара.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Цена товара.
+        /// </summary>
+        public int Price { get; set; }
+    }
+}
---
+++ Manual.Testers.Api/Manual.Testers.Api.Host/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Manual.Testers.Api.Host.Models
+{
+    /// <summary>
+    /// Заказ.
+    /// </summary>
+    public class Order
+    {
+        /// <summary>
+        /// Дата заказа.
+        /// </summary>
+        public string OrderDate { get; set; }
+
+        /// <summary>
+        /// Список товаров с ценами.
+        /// </summary>
+        public List<Good> Goods { get; set; }
+    }
+}
